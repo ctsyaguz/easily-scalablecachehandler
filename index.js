ac3a76669f065c05cf31c112a68984f961446fca
ctsyaguz <psjcinguiboy220@hotmail.com>
2024-05-05 11:29:36 +0700

    test

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-function combinationSum(candidates, target) {
-  const result = [];
-  backtrack([], 0, 0);
-  return result;
-  function backtrack(combination, start, sum) {
-    if (sum === target) {
-      result.push([...combination]);
-      return;
-    }
-    if (sum > target) return;
-    for (let i = start; i < candidates.length; i++) {
-      combination.push(candidates[i]);
-      backtrack(combination, i, sum + candidates[i]);
-      combination.pop();
-    }
-  }
-}
+const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
