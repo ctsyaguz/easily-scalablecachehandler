23fdb4caf012f14caf63dca1bddcbf98a9898d14
ctsyaguz <psjcinguiboy220@hotmail.com>
2024-05-07 19:03:03 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-function findCircleNum(M) {
-  if (!M || M.length === 0) return 0;
-  const visited = new Array(M.length).fill(false);
-  let count = 0;
-  for (let i = 0; i < M.length; i++) {
-    if (!visited[i]) {
-      dfs(M, i, visited);
-      count++;
-    }
+function maxProduct(nums) {
+  if (nums.length === 0) return 0;
+  let maxSoFar = nums[0];
+  let minSoFar = nums[0];
+  let maxProduct = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    const temp = maxSoFar;
+    maxSoFar = Math.max(nums[i], nums[i] * maxSoFar, nums[i] * minSoFar);
+    minSoFar = Math.min(nums[i], nums[i] * temp, nums[i] * minSoFar);
+    maxProduct = Math.max(maxProduct, maxSoFar);
   }
-  return count;
-}
-function dfs(M, i, visited) {
-  visited[i] = true;
-  for (let j = 0; j < M.length; j++) {
-    if (M[i][j] === 1 && !visited[j]) {
-      dfs(M, j, visited);
-    }
-  }
+  return maxProduct;
 }
